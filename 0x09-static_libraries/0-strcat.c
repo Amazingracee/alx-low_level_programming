6015c240b4d3ac01f59e49fcbaf4512cd43ab561
Grace <olabodephoeb@gmail.com>
2022-09-08 04:43:43 -0700

    Solutions

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenate two strings
- * @dest: First string
- * @src: Second string
- *
- * Return: Pointer to resulting string dest
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (c = 0; src[c] != '\0'; c++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[c];
 		i++;
 	}
 
